c9fed7308b107e7d909189e9b1cbe50d5cc5e793
Amir <playergood0@live.com>
2018-07-16 16:53:42 +0430

    onStart and onStop replaced with onResume and onPause

--- app/src/main/java/com/amir/stopwatch/StopwatchActivity.java
+++ app/src/main/java/com/amir/stopwatch/StopwatchActivity.java
@@ -11,7 +11,8 @@ import java.util.Locale;
 public class StopwatchActivity extends Activity {
 
     private int seconds = 0;
-    private boolean running = false;
+    private boolean running;
+    private boolean wasRunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +21,31 @@ public class StopwatchActivity extends Activity {
         if (savedInstanceState != null) {
             seconds = savedInstanceState.getInt("seconds");
             running = savedInstanceState.getBoolean("running");
+            wasRunning = savedInstanceState.getBoolean("wasRunning");
         }
         runTimer();
     }
+
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
         savedInstanceState.putInt("seconds", seconds);
         savedInstanceState.putBoolean("running", running);
+        savedInstanceState.putBoolean("wasRunning", wasRunning);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        wasRunning = running;
+        running = false;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (wasRunning) {
+            running = true;
+        }
     }
 
     public void onClickStart(View view) {
